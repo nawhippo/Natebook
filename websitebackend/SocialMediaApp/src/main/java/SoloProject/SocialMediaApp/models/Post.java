dc10c40e1a8abfb5cc9029c7c09cb42d1bc16ccf
NATHAN <whipmiester@hotmail.com>
2023-08-21 09:33:30 -0400

    added important fields

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/models/Post.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/models/Post.java
@@ -1,13 +1,56 @@
 package SoloProject.SocialMediaApp.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "posts")
 public class Post {
+    public Post(Long id, AppUser appUser, String posterusername, String title, String description, List<Long> likes, List<Long> dislikes, List<Comment> commentList) {
+        this.id = id;
+        this.appUser = appUser;
+        this.posterusername = posterusername;
+        this.title = title;
+        this.description = description;
+        this.likes = new ArrayList<>();
+        this.dislikes = new ArrayList<>();
+        this.commentList = new ArrayList<>();
+        this.dateTime =  getDateTimeAsString();
+    }
+
+    public List<Long> addLike(Long userId){
+        likes.add(userId);
+        return likes;
+    }
+
+    public List<Long> addDislike(Long userId){
+        dislikes.add(userId);
+        return dislikes;
+    }
+    public List<Long> removeLike(Long userId){
+        likes.remove(userId);
+        return likes;
+    }
+
+    public List<Long> removeDislike(Long userId){
+        dislikes.remove(userId);
+        return dislikes;
+    }
+
+
+
+    public String getPosterusername() {
+        return posterusername;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,22 +60,51 @@ public class Post {
     @JsonIgnore
     private AppUser appUser;
 
+    public void setPosterusername(String posterusername) {
+        this.posterusername = posterusername;
+    }
+
+    //for display purposes
+   private String posterusername;
     @Column
     private String title;
 
     @Column
     private String description;
 
+
+    //return the length of this to get number of.
     @Column
-    private int likes;
+    private List<Long> likes;
 
     @Column
-    private int dislikes;
+    private List<Long> dislikes;
 
-    @Temporal(TemporalType.TIMESTAMP)
+
+
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> commentList;
+
+    public void addComment(Comment comment) {
+        commentList.add(comment);
+        comment.setPost(this);
+    }
+
+    public void removeComment(Comment comment){
+        commentList.remove(comment);
+        comment.setPost(null);
+    }
+
+
     @Column
-    @JsonFormat(pattern = "MM-dd-yyyy 'at' hh:mm:ss a")
-    private Date dateTime;
+    private String dateTime;
+
+
+    public String getDateTimeAsString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy 'at' hh:mm:ss a");
+        return sdf.format(new Date());
+    }
 
     public Long getId() {
         return id;
@@ -62,31 +134,39 @@ public class Post {
         this.description = description;
     }
 
-    public int getLikes() {
+    public List<Long> getLikes() {
         return likes;
     }
 
-    public void setLikes(int likes) {
+    public void setLikes(List<Long> likes) {
         this.likes = likes;
     }
 
-    public int getDislikes() {
+    public List<Long> getDislikes() {
         return dislikes;
     }
 
-    public void setDislikes(int dislikes) {
+    public void setDislikes(List<Long> dislikes) {
         this.dislikes = dislikes;
     }
 
-    public Date getDateTime() {
+    public String getDateTime() {
         return dateTime;
     }
 
-    public void setDateTime(Date dateTime) {
+    public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
     }
 }
