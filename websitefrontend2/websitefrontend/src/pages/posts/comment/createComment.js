26def18c7bb714d05a996b32e28ab8ddb94789e8
NATHAN <whipmiester@hotmail.com>
2023-08-21 09:34:27 -0400

    added react for create comment page.

---
+++ websitefrontend2/websitefrontend/src/pages/posts/comment/createComment.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { useUserContext } from '../../login/UserContext';
+const CommentForm = ({ userId, postId }) => {
+  const { user } = useUserContext();
+  const [commentContent, setCommentContent] = useState('');
+
+  const createComment = async () => {
+    const response = await fetch(`/api/post/${user.appUserID}/${postId}/createComment`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: commentContent,
+      }),
+    });
+
+    if (response.ok) {
+      console.log('Comment created successfully');
+    } else {
+      console.error('Failed to create comment');
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await createComment();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Comment:
+          <input
+            type="text"
+            value={commentContent}
+            onChange={(e) => setCommentContent(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentForm;
