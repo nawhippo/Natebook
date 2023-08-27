8fde6b633910e3171f410cd2a3ad92aa05830dff
NATHAN <whipmiester@hotmail.com>
2023-08-26 23:32:31 -0400

    added remove friend endpoint

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/controller/FriendController.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/controller/FriendController.java
@@ -7,10 +7,7 @@ import SoloProject.SocialMediaApp.service.AppUserService;
 import SoloProject.SocialMediaApp.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,12 @@ public class FriendController {
         this.friendService = friendService;
         this.appUserRepository = appUserRepository;
     }
+
+    @DeleteMapping("/friends/{userId}/{friendusername}/removeFriend")
+        public ResponseEntity<AppUser> removeFriend(@PathVariable Long userId, @PathVariable String friendusername) {
+            return friendService.removeFriend(userId, friendusername);
+        }
+
 
     @GetMapping("/friends/{userId}/getAllFriends")
     public ResponseEntity<List<UserDTO>> getAllFriends(@PathVariable Long userId) {
