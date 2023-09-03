2d5e8513a584d0e405f6f36ebdc744f9874a4c2a
NATHAN <whipmiester@hotmail.com>
2023-09-03 13:31:26 -0400

    added address book endpoint

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/controller/AddressBookController.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/controller/AddressBookController.java
@@ -1,7 +1,9 @@
 package SoloProject.SocialMediaApp.controller;
 
 import SoloProject.SocialMediaApp.models.AppUser;
+import SoloProject.SocialMediaApp.models.Post;
 import SoloProject.SocialMediaApp.service.AppUserService;
+import SoloProject.SocialMediaApp.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,20 +14,24 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api") // The base path for all endpoints in this controller
-public class HomeController {
+public class AddressBookController {
 
-    private final AppUserService userserviceimpl;
-
+    private final AppUserService userService;
+    private final PostService postService;
     @Autowired
-    public HomeController(AppUserService userserviceimpl) {
-        this.userserviceimpl = userserviceimpl;
+    public AddressBookController(AppUserService userservice, PostService postservice) {
+        this.userService = userservice;
+        this.postService = postservice;
     }
 
     @GetMapping("/home")
     public ResponseEntity<List<AppUser>> getHomePageData() {
-        return userserviceimpl.getAllUsers();
+        return userService.getAllUsers();
     }
 
+
+    @GetMapping("/address-book")
+    public ResponseEntity<List<Post>> getAllPublicPosts() { return postService.getAllPublicPosts();}
     @GetMapping("/about") // The endpoint URL: /api/about
     public String about() {
         return "{\"message\": \"Welcome to my about page, it is a react/java application. It is a simple CRUD application, " +
