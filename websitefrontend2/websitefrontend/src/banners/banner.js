834c57e503f38cc4b81bd9c8f9f5d56dfcb62a31
NATHAN <whipmiester@hotmail.com>
2023-09-04 22:06:30 -0400

    added drop down menu to banner

--- websitefrontend2/websitefrontend/src/banners/banner.js
+++ websitefrontend2/websitefrontend/src/banners/banner.js
@@ -1,35 +1,64 @@
-import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import './banner.css'; // Import the stylesheet
-
+import React, { useContext }  from 'react';
+import { useHistory } from 'react-router-dom';
+import './banner.css';
+import { useUserContext } from '../pages/usercontext/UserContext';
+import { UserContext } from '../pages/usercontext/UserContext';
 const Banner = () => {
   const history = useHistory();
-
+  const { user, setUser, clearUserContext } = useUserContext();
+  const isLoggedIn = !!user;
   const handleLinkClick = (url) => {
-    //navigate to the specified URL, causing a page refresh in a stupid-roundabout way. 
     history.push(url);
   };
 
   return (
     <div className="banner">
-      {/* Add your banner content here */}
       <h1>NateBook</h1>
-      {/* Links should be wrapped within a Router component */}
-      <button onClick={() => handleLinkClick('/getAllFriends')}>View Friends List</button>
-      <button onClick={() => handleLinkClick('/getAllPosts')}>View Posts</button>
-      <button onClick={() => handleLinkClick('/getFriendRequests')}>View Friend Requests</button>
-      <button onClick={() => handleLinkClick('/getAllMessages')}>View Messages</button>
-      <button onClick={() => handleLinkClick('/about')}>About</button>
+
+      <div className="dropdown">
+        <button className="dropbtn">Account Management</button>
+        <div className="dropdown-content">
+          {isLoggedIn ? (
+          <>
+          <p onClick={() => handleLinkClick('/logout')}>Logout</p>
+          <p onClick={() => handleLinkClick('/accountDetails')}>Account Details</p>
+          </>
+           ) : ( 
+            <>
+            <p onClick={() => handleLinkClick('/createAccount')}>Create Account</p>
+            <p onClick={() => handleLinkClick('/login')}>Login</p>
+            </>
+           )}
+        </div>
+      </div>
+
+      <div className="dropdown">
+        <button className="dropbtn">Friends</button>
+        <div className="dropdown-content">
+          <p onClick={() => handleLinkClick('/getAllFriends')}>View Friends List</p>
+          <p onClick={() => handleLinkClick('/getFriendRequests')}>View Friend Requests</p>
+          <p onClick={() => handleLinkClick('/sendFriendRequest')}>Send Friend Request</p>
+        </div>
+      </div>
+
+      <div className="dropdown">
+        <button className="dropbtn">Posts</button>
+        <div className="dropdown-content">
+          <p onClick={() => handleLinkClick('/getAllPosts')}>View Posts</p>
+          <p onClick={() => handleLinkClick('/createPost')}>Create Post</p>
+        </div>
+      </div>
+
       <button onClick={() => handleLinkClick('/home')}>Home</button>
-      <button onClick={() => handleLinkClick('/login')}>Login</button>
-      <button onClick={() => handleLinkClick('/logout')}>Log Out</button>
-      <button onClick={() => handleLinkClick('/createAccount')}>Create Account</button>
-      <button onClick={() => handleLinkClick('/accountDetails')}>Account Details</button>
-      <button onClick={() => handleLinkClick('/createPost')}>Create Post</button>
-      <button onClick={() => handleLinkClick('/createMessage')}>Send Message</button>
-      <br></br>
-      <button onClick={() => handleLinkClick('/addFriend')}>Add Friend</button>
-      <button onClick={() => handleLinkClick('/sendFriendRequest')}>Send Friend Request</button>
+      <button onClick={() => handleLinkClick('/about')}>About</button>
+
+      <div className="dropdown">
+        <button className="dropbtn">Messages</button>
+        <div className="dropdown-content">
+          <p onClick={() => handleLinkClick('/getAllMessages')}>View Messages</p>
+          <p onClick={() => handleLinkClick('/createMessage')}>Send Message</p>
+        </div>
+      </div>
     </div>
   );
 };
