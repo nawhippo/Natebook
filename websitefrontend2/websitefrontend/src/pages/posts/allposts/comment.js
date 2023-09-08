7dfff5af7e9acee9da3b8376ea2b1214e1cb8d28
NATHAN <whipmiester@hotmail.com>
2023-09-08 13:15:11 -0400

    separating comment/post objects into seperate files for simplicity's sake

---
+++ websitefrontend2/websitefrontend/src/pages/posts/allposts/comment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//to shorten posts page.
+const Comment = ({ comment, user, postId, handleReaction, handleDeleteComment }) => {
+  return (
+    <div key={comment.id} className='comment'>
+      <p>{comment.content}</p>
+      <p>By: {comment.commenterusername}</p>
+      <p><button onClick={() => handleReaction("comment", postId, postId, comment.id, "Like")}>Like</button>
+         <button onClick={() => handleReaction("comment", postId, postId, comment.id, "Dislike")}>Dislike</button></p>
+      {comment.commenterId === user.appUserID && 
+        <button onClick={() => handleDeleteComment(user.appUserID, postId, comment.id)}>Delete Comment</button>}
+    </div>
+  );
+};
+
+export default Comment;
---
+++ websitefrontend2/websitefrontend/src/pages/posts/allposts/post.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//to shorten posts page.
+const Post = ({ post, user, handleReaction, handleDeletePost, handleCommentClick, currentPostId, CommentFormComponent }) => {
+  return (
+    <div key={post.id} className="post-card">
+      <h2>{post.title}</h2>
+      <p>{post.description}</p>
+      <p><button onClick={() => handleReaction("post", post.id, post.posterid, null, "Like")}>Like</button>
+         <button onClick={() => handleReaction("post", post.id, post.posterid, null, "Dislike")}>Dislike</button></p>
+      <p>{post.email}</p>
+      <p>{post.dateTime}</p>
+      <p>By: {post.posterusername}</p>
+      {post.posterid === user.appUserID && 
+        <button onClick={() => handleDeletePost(user.appUserID, post.id)}>Delete Post</button>}
+      
+      <p>Comments:</p>
+      <button onClick={() => handleCommentClick(post.id)}>Comment</button>
+      {currentPostId === post.id && <CommentFormComponent postId={post.id} />}
+    </div>
+  );
+};
+
+export default Post;
