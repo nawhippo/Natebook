b688e3bcfaf14449a8bdacaef6e504ba0eb6bc9a
NATHAN <whipmiester@hotmail.com>
2023-09-09 20:57:37 -0400

    factored out functions into buttons

--- websitefrontend2/websitefrontend/src/pages/posts/allposts/getAllPostsByUsername.js
+++ websitefrontend2/websitefrontend/src/pages/posts/allposts/getAllPostsByUsername.js
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useUserContext } from '../../usercontext/UserContext';
-import CommentForm from '../comment/createComment';
-import Post from './post';
-import Comment from './comment';
+import Post from '../post';
+
 const PostsPage = () => {
   const { user } = useUserContext();
-  const [targetUsername, setTargetUsername] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [inputValue, setInputValue] = useState('');
-  const [currentPostId, setCurrentPostId] = useState(null);
   const [allPostsData, setAllPostsData] = useState(null);
-  const [showCommentForm, setShowCommentForm] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -20,42 +14,20 @@ const PostsPage = () => {
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
-  
+
   const handleSearchClick = () => {
     fetchData(inputValue);
   };
 
-  const handleCommentClick = async (postid, comment) => {
-    if (comment) {
-    const endpoint = `/api/post/${user.appUserID}/${postid}/createComment`;
-    try {
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ comment }),
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to create comment.');
-      }
-  
-      fetchData(targetUsername); // refresh data after commenting
-    } catch (error) {
-      setError(error.message);
-    }
-    setShowCommentForm(false);
-  } else {
-    setShowCommentForm(!showCommentForm);
-  }
-  };
-
   const fetchData = async (event) => {
+    if (!user) {
+      console.error("User is null");
+      return;
+    }
     const endpoint = event 
       ? `/api/post/${user.appUserID}/postsByUsername/${event}`
       : `/api/post/${user.appUserID}/friendPosts`;
-    setIsLoading(true);
+
     try {
       const response = await fetch(endpoint);
       if (!response.ok) {
@@ -64,107 +36,19 @@ const PostsPage = () => {
       const data = await response.json();
       setAllPostsData(data);
     } catch (err) {
-      setError(err.message);
-    } finally {
-      setIsLoading(false);
+      console.error(err);
     }
   };
 
-  const handleDeleteComment = async (userId, postId, commentId) => {
-    try {
-      const response = await fetch(`/api/post/${userId}/${postId}/${commentId}/deleteComment`, { method: 'DELETE' });
-      if (!response.ok) {
-        throw new Error('Failed to delete.');
+  return (
+    <div>
+      <h1>Friend Posts</h1>
+      <input type="text" value={inputValue} placeholder="Enter username" onChange={handleInputChange} />
+      <button onClick={handleSearchClick}>Search</button>
+      {allPostsData && allPostsData.length > 0 
+        ? allPostsData.map((post) => <Post key={post.id} post={post} user={user} />)
+        : <p>No posts found.</p>}
+    </div>
+  );
       }
-      fetchData(targetUsername);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  const handleDeletePost = async (userId, postId) => {
-    try {
-      const response = await fetch(`/api/post/${userId}/${postId}/deletePost`, { method: 'DELETE' });
-      if (!response.ok) {
-        throw new Error('Failed to delete.');
-      }
-      fetchData(targetUsername);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-  
-
-  const handleReaction = async (type, postId, posterId, commentId = null, action) => {
-    let updateReactionEndpoint;
-  
-    if (type === "post") {
-      updateReactionEndpoint = `/api/post/${user.appUserID}/${posterId}/${postId}/updateReactionPost`;
-    } else if (type === "comment") {
-      updateReactionEndpoint = `/api/post/${user.appUserID}/${posterId}/${postId}/${commentId}/updateReactionComment`;
-    } else {
-      console.error("Invalid reaction type");
-      return;
-    }
-  
-    try {
-        const response = await fetch(updateReactionEndpoint, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ action }),
-        });
-    
-        if (!response.ok) {
-          throw new Error("Failed to update reaction.");
-        }
-    
-        // Refresh the post data
-        fetchData(targetUsername);
-      } catch (error) {
-        setError(error.message);
-      }
-
-};
-
-return (
-  <div>
-    <h1>Friend Posts</h1>
-    <input type="text" value={inputValue} placeholder="Enter username" onChange={handleInputChange} />
-    <button onClick={handleSearchClick}>Search</button>
-
-    {allPostsData && allPostsData.length > 0 ? allPostsData.map((post) => (
-      <div key={post.id}>
-        <Post
-          post={post}
-          user={user}
-          handleReaction={handleReaction}
-          handleDeletePost={handleDeletePost}
-          handleCommentClick={() => handleCommentClick(post.id)}
-          currentPostId={currentPostId}
-          CommentFormComponent={CommentForm}
-        >
-          {post.commentList.map((comment) => (
-            <Comment
-              comment={comment}
-              user={user}
-              postId={post.id}
-              handleReaction={handleReaction}
-              handleDeleteComment={handleDeleteComment}
-            />
-          ))}
-        </Post>
-        {showCommentForm && currentPostId === post.id ? (
-          <CommentForm
-            onSubmit={(comment) => {
-              handleCommentClick(post.id, comment);
-            }}
-          />
-        ) : null}
-      </div>
-    )) : <p>No posts found.</p>}
-  </div>
-);
-          }
-export default PostsPage;
+  export default PostsPage;
