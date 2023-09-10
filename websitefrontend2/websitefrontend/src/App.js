62d2ca4533d9d81d56e8823f53af1527296e00d3
NATHAN <whipmiester@hotmail.com>
2023-09-09 20:51:42 -0400

    ...

--- websitefrontend2/websitefrontend/src/App.js
+++ websitefrontend2/websitefrontend/src/App.js
@@ -7,10 +7,9 @@ import Login from './pages/usercontext/Login';
 import Home from './pages/home/Home';
 import createAccount from './pages/account/createAccount/createAccount';
 import getAllPostByUsername from './pages/posts/allposts/getAllPostsByUsername';
-import specPost from './pages/posts/specpost/specPost';
 import specFriend from './pages/friends/specfriend/specFriend';
 import GetAllFriends from './pages/friends/allfriends/getAllFriends';
-import createMessage from './pages/message/createMessage/createMessage';
+import createMessage from './pages/buttonComponents/sendMessageButton/createMessageButton';
 import createPost from './pages/posts/createPost/createPost';
 import ProtectedRoute from './pages/usercontext/ProtectedRoute';
 import Banner from './banners/banner';
@@ -19,12 +18,12 @@ import Logout from './pages/usercontext/Logout';
 import SendFriendRequestByUsername from './pages/friendrequests/SendFriendRequest/SendFriendRequestByUsername';
 import GetAllFriendRequests from './pages/friendrequests/ViewAllFriendRequests/ViewAllFriendRequests';
 import GetAccount from './pages/account/getAccount/getAccount';
-import GetAllMessages from './pages/message/getAllMessages/getAllMessages';
-import CommentForm from './pages/posts/comment/createComment';
+import GetAllMessages from './pages/message/getAllMessages/MessagesPage';
+import CommentForm from './pages/posts/comment/createComment/createComment';
 import UpdateAccount from './pages/account/updateAccount/updateAccount';
 import PostsPage from './pages/posts/allposts/getAllPostsByUsername';
 import DeleteAccount from './pages/account/deleteAccount/deleteAccount';
-import GetUser from './pages/profiles/UserProfile';
+import GetUser from './pages/profile/profilePage';
 class App extends Component {
   render() {
     return (
@@ -45,7 +44,6 @@ class App extends Component {
                 <Route path="/specFriend/:userId" component={specFriend} /> 
                 <Route path="/createMessage" component={createMessage} />
                 <Route path="/login" component={Login} />
-                <Route path="/specPost/:userId" component={specPost} /> 
                 <Route path="/getAllPosts" component={PostsPage} />
                 <Route path="/createPost" component={createPost} />
                 <Route path="/sendFriendRequestByUsername" component={SendFriendRequestByUsername} />
