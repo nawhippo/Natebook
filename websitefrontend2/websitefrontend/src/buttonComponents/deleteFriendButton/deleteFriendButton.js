3dbf8c5ab34241060b734b511ff8e4d6addeb2f9
NATHAN <whipmiester@hotmail.com>
2023-09-10 21:17:36 -0400

    added

---
+++ websitefrontend2/websitefrontend/src/buttonComponents/deleteFriendButton/deleteFriendButton.js
@@ -0,0 +1,20 @@
+const deleteFriendButton = ({ username, removeFriend }) => {
+    const handleClick = () => {
+      fetch(`/api/friends/${username}/removeFriend`, {
+        method: 'DELETE',
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        removeFriend(username);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+    };
+  
+    return <button onClick={handleClick}>Remove Friend</button>;
+  };
+  
+  export default deleteFriendButton;
