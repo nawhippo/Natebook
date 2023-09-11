d682b5fa773b72368a70594496a7c053c59c0981
NATHAN <whipmiester@hotmail.com>
2023-09-10 21:27:28 -0400

    added

---
+++ websitefrontend2/websitefrontend/src/banners/websiteBanner.js
@@ -0,0 +1,42 @@
+import LoginButton from '../buttonComponents/loginButton/loginButton';
+import LogoutButton from '../buttonComponents/logoutButton/logoutButton';
+import CreateAccount from '../buttonComponents/createAccountButton/createAccountButton';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useUserContext } from '../pages/usercontext/UserContext';
+import './banner.css'
+
+
+const Banner = () => {
+  const history = useHistory();
+  const { user } = useUserContext(); 
+  const isLoggedIn = !!user;
+
+  const handleLinkClick = (url) => {
+    history.push(url);
+  };
+
+  return (
+    <div className="banner">
+      <h1>NateBook</h1>
+      <button onClick={() => handleLinkClick('/AddressBook')}>Home</button>
+      <button onClick={() => handleLinkClick('/Account')}>Account</button>
+      <button onClick={() => handleLinkClick('/Messages')}>View Messages</button>
+      <button onClick={() => handleLinkClick('/Feed')}>View Feed</button>
+      <button onClick={() => handleLinkClick('/Friends')}>Friends</button>
+
+      {isLoggedIn ? (
+        <div>
+        <p>Logged in as : {user.username} </p>
+        <LogoutButton />
+        </div>
+      ) : (
+        <>
+          <LoginButton />
+          <CreateAccount />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Banner;
