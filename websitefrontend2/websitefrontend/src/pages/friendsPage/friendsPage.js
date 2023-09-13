ee6d62af9106bd10c0618c76769154baad70647e
NATHAN <whipmiester@hotmail.com>
2023-09-12 23:36:08 -0400

    added divs

--- websitefrontend2/websitefrontend/src/pages/friendsPage/friendsPage.js
+++ websitefrontend2/websitefrontend/src/pages/friendsPage/friendsPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './allfriends.css'
 import GetAllFriendRequests from "./helperComponents/getAllFriendRequests"
 import GetAllFriends from "./helperComponents/getAllFriends"
 const FriendsPage = () => {
   return (
     <div>
       <h1>Friends Page</h1>
+      <div className="friends-container">
       <GetAllFriends />
       <GetAllFriendRequests />
+      </div>
     </div>
   );
 };
