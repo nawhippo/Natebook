c0b9df27661d247d231f8b1572464aa46cffa7ef
NATHAN <whipmiester@hotmail.com>
2023-09-12 23:46:16 -0400

    added dynamically updating likes/dislikes

--- websitefrontend2/websitefrontend/src/buttonComponents/reactCommentButtons/reactCommentButtons.js
+++ websitefrontend2/websitefrontend/src/buttonComponents/reactCommentButtons/reactCommentButtons.js
@@ -1,30 +1,48 @@
-const ReactionButtons = ({ user, postId, posterusername, commentId, handleReaction }) => {
-  const handleButtonClick = async (action) => {
-    try {
-      const response = await fetch(`/post/${user.appUserID}/${posterusername}/${postId}/${commentId}/updateReactionComment`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ action }),
-      });
+import React, { useState } from 'react';
 
-      if (!response.ok) {
-        throw new Error('Failed to update reaction');
+const ReactionButtons = ({ user, postId, posterId, commentId }) => {
+  const [reactionState, setReactionState] = useState('None');
+  const [localLikesCount, setLocalLikesCount] = useState(0);
+  const [localDislikesCount, setLocalDislikesCount] = useState(0);
+
+  const updateReactionOnServer = async (reactorId, posterId, postId, commentId, action) => {
+    const url = `/api/post/${reactorId}/${posterId}/${postId}/${commentId}/updateReactionComment`;
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ action }),
+    });
+
+    if (response.ok) {
+      const updatedComment = await response.json();
+      return updatedComment;
+    } else {
+      return null;
+    }
+  };
+
+  const handleButtonClick = async (newReaction) => {
+    const updatedComment = await updateReactionOnServer(user.appUserID, posterId, postId, commentId, newReaction);
+    if (updatedComment) {
+      if (newReaction === reactionState) {
+        setReactionState('None');
+      } else {
+        setReactionState(newReaction);
       }
-
-      // Refresh data or do some other action
-      handleReaction('comment', postId, posterusername, commentId, action);
-    } catch (error) {
-      console.error('Error updating reaction:', error);
+      setLocalLikesCount(updatedComment.likesCount);
+      setLocalDislikesCount(updatedComment.dislikesCount);
     }
   };
 
   return (
     <div>
+      <p>Likes: {localLikesCount} Dislikes: {localDislikesCount}</p>
       <button onClick={() => handleButtonClick('Like')}>Like</button>
       <button onClick={() => handleButtonClick('Dislike')}>Dislike</button>
     </div>
   );
 };
+
 export default ReactionButtons;
