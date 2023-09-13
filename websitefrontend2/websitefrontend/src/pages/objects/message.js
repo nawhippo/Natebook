076c3ef6137123ecef4e687f607d9f990606b279
NATHAN <whipmiester@hotmail.com>
2023-09-12 23:38:39 -0400

    added children message display

--- websitefrontend2/websitefrontend/src/pages/objects/message.js
+++ websitefrontend2/websitefrontend/src/pages/objects/message.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Message = ({ message, user }) => {
+  const [showReplies, setShowReplies] = useState(false);
+
+  const toggleReplies = () => {
+    setShowReplies(!showReplies);
+  };
+
   return (
     <div>
       <p>{message.title}</p>
@@ -8,6 +14,18 @@ const Message = ({ message, user }) => {
       <p>Sender: {message.incoming ? user.username : 'You'}</p>
       {message.recipients && (
         <p>Recipients: {message.recipients.join(', ')}</p>
+      )}
+      <button onClick={toggleReplies}>Reply</button>
+      {showReplies && message.childMessages && (
+        <div>
+          <h4>Replies:</h4>
+          {message.childMessages.map((reply, index) => (
+            <div key={index}>
+              <p>{reply.content}</p>
+              <p>Sender: {reply.incoming ? reply.sender.username : 'You'}</p>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
