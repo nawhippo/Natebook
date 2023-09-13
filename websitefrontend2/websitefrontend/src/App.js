4d61cc6403129a87913406679ade778d3d534d75
NATHAN <whipmiester@hotmail.com>
2023-09-12 23:22:39 -0400

    altered endpoint

--- websitefrontend2/websitefrontend/src/App.js
+++ websitefrontend2/websitefrontend/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import About from './pages/about/About';
 import { UserProvider } from './pages/usercontext/UserContext';
 import { useUserContext } from './pages/usercontext/UserContext';
 import FeedPage from './pages/feedPage/feedPage';
 import AccountPage from './pages/accountPage/accountPage';
 import FriendsPage from "./pages/friendsPage/friendsPage";
-import AddressBookPage from './pages/addressbookPage/AddressBookPage';
+import AllUserPage from './pages/allUsersPage/allUsersPage';
 import MessagesPage from './pages/messagePage/MessagesPage';
 import ProfilePage from './pages/profilePage/profilePage';
 import Banner from "./banners/websiteBanner"
@@ -21,9 +20,9 @@ class App extends Component {
               <Switch>
                 <Route path="/Account" exact component={AccountPage} />
                 <Route path="/Feed" exact component={FeedPage} />
-                <Route path ="/AddressBook" exact component={AddressBookPage} />
+                <Route path ="/AllUsersPage" exact component={AllUserPage} />
                 <Route path="/Friends" component={FriendsPage} />
-                <Route path="/UserProfile/:username" component={ProfilePage}/>
+                <Route path="/UserProfile/:userid" component={ProfilePage}/>
                 <Route path="/Messages" component={MessagesPage}/>
               </Switch>    
             </header>
