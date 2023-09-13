faddd1100dcc15bf84ad350d1e2c6b874ce5cd68
NATHAN <whipmiester@hotmail.com>
2023-09-12 23:27:41 -0400

    added dynamically updating like count

--- websitefrontend2/websitefrontend/src/pages/objects/comment.js
+++ websitefrontend2/websitefrontend/src/pages/objects/comment.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DeleteCommentButton } from '../../buttonComponents/deleteCommentButton/deleteCommentButton';
 import ReactionButtons from '../../buttonComponents/reactCommentButtons/reactCommentButtons';
-const Comment = ({ comment, user, postId, posterusername, handleReaction, fetchData }) => {
+
+const Comment = ({ comment, user, postId, posterId, fetchData }) => {
+  const [localLikesCount, setLocalLikesCount] = useState(comment.likesCount);
+  const [localDislikesCount, setLocalDislikesCount] = useState(comment.dislikesCount);
+
+  const updateLocalLikesDislikes = (newLikes, newDislikes) => {
+    setLocalLikesCount(newLikes);
+    setLocalDislikesCount(newDislikes);
+  };
+
   return (
     <div key={comment.id} className='comment'>
       <p>{comment.content}</p>
       <p>By: {comment.commenterusername}</p>
-      
       <ReactionButtons 
         user={user} 
         postId={postId} 
-        posterusername={posterusername} 
+        posterId={posterId} 
         commentId={comment.id} 
-        handleReaction={handleReaction} 
+        updateCommentLikesDislikes={updateLocalLikesDislikes}
       />
       
-      {user && (comment.commenterusername === user.username || posterusername === user.username) && 
+      {user && (comment.commenterusername === user.username || posterId === user.appUserID) && 
         <DeleteCommentButton 
           commenterusername={user.username} 
           postId={postId} 
