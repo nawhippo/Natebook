1127caca6b36af5f950cf9b36d42296d8c9072a8
NATHAN <whipmiester@hotmail.com>
2023-09-16 22:15:08 -0400

    WIP for new endpoints

--- websitefrontend2/websitefrontend/src/pages/messagePage/MessagesPage.js
+++ websitefrontend2/websitefrontend/src/pages/messagePage/MessagesPage.js
@@ -3,10 +3,11 @@ import { useUserContext } from '../usercontext/UserContext';
 import Message from '../objects/message';
 import ReplyMessageForm from '../../buttonComponents/replyToMessageButton/replyToMessageButton';
 import CreateMessageForm from '../../buttonComponents/createMessageButton/createMessageButton';
+
 const MessagesPage = () => {
   const { user } = useUserContext();
-  const [showIncoming, setShowIncoming] = useState(true);
   const [searchText, setSearchText] = useState('');
+  const [showIncoming, setShowIncoming] = useState(true);
   const [allMessages, setAllMessages] = useState([]);
   const [displayedMessages, setDisplayedMessages] = useState([]);
 
@@ -15,50 +16,47 @@ const MessagesPage = () => {
       .then((response) => response.json())
       .then((data) => {
         setAllMessages(data);
-        setDisplayedMessages(data);
+        filterMessages(searchText, data);
       })
       .catch((error) => console.error('Error fetching messages:', error));
   }, [user]);
 
   const handleSearchTextChange = (event) => {
-    const newSearchText = event.target.value;
-    setSearchText(newSearchText);
-    filterMessages(newSearchText);
+    const textToSearch = event.target.value;
+    setSearchText(textToSearch);
+    filterMessages(textToSearch, allMessages);
   };
 
-  const filterMessages = (textToSearch) => {
-    const filteredMessages = allMessages.filter((message) =>
-      message.content.toLowerCase().includes(textToSearch.toLowerCase()) ||
-      (message.title && message.title.toLowerCase().includes(textToSearch.toLowerCase())) ||
-      (message.recipients && message.recipients.join(', ').toLowerCase().includes(textToSearch.toLowerCase()))
-    );
-    setDisplayedMessages(filteredMessages);
+  const filterMessages = (textToSearch, messages) => {
+    const filtered = messages.filter((message) => {
+      return message.content.toLowerCase().includes(textToSearch.toLowerCase());
+    });
+    setDisplayedMessages(filtered);
   };
 
-  const messagesToDisplay = showIncoming 
-    ? displayedMessages.filter((message) => message.isIncoming)
-    : displayedMessages.filter((message) => !message.isIncoming);
+  const messagesToDisplay = displayedMessages.filter((message) => {
+    return showIncoming ? message.isIncoming : !message.isIncoming;
+  });
 
   return (
     <div>
       <h1>Messages Page</h1>
-      <CreateMessageForm userId = {user.appUserID}></CreateMessageForm>
-      <input
+      <CreateMessageForm userId={user.appUserID}></CreateMessageForm>
+      <input 
         type="text"
+        placeholder="Search messages..."
         value={searchText}
         onChange={handleSearchTextChange}
-        placeholder="Search Messages"
       />
-      
-      <button onClick={() => setShowIncoming(!showIncoming)}> 
-        {showIncoming ? 'Show Sent Messages' : 'Show Incoming Messages'} 
+      <button onClick={() => setShowIncoming(!showIncoming)}>
+        {showIncoming ? 'Show Sent Messages' : 'Show Incoming Messages'}
       </button>
       <h2>{showIncoming ? 'Incoming Messages:' : 'Sent Messages:'}</h2>
       <ul>
         {messagesToDisplay.map((message) => (
           <li key={message.id}>
             <Message message={message} user={user} />
-            <ReplyMessageForm userId={user.appUserID} parentSenderId={message.senderId} messageId={message.id} /> 
+            <ReplyMessageForm messageId={message.id} posterId={message.senderid} userId={user.appUserID}/>
           </li>
         ))}
       </ul>
