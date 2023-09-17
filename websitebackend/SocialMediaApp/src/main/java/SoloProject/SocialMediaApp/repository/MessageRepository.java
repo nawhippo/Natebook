b47a7f8444bc01b78a2c4e1ed7083ca91c9b2778
NATHAN <whipmiester@hotmail.com>
2023-09-16 22:14:02 -0400

    added message repo for easier access.

---
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package SoloProject.SocialMediaApp.repository;
+
+import SoloProject.SocialMediaApp.models.Message;
+import SoloProject.SocialMediaApp.models.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByRecipientsContains(String username);
+    List<Message> findBySender(AppUser sender);
+}
