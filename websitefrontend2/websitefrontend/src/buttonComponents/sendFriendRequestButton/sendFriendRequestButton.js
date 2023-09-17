7b0be0fe720a4deac6239093b6a44c0fe2faa433
NATHAN <whipmiester@hotmail.com>
2023-09-16 22:17:25 -0400

    added friendreq notification

--- websitefrontend2/websitefrontend/src/buttonComponents/sendFriendRequestButton/sendFriendRequestButton.js
+++ websitefrontend2/websitefrontend/src/buttonComponents/sendFriendRequestButton/sendFriendRequestButton.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useUserContext } from '../../pages/usercontext/UserContext';
 
 const SendFriendRequestButton = ({ username }) => {
   const { user } = useUserContext();
+  const [sent, setSent] = useState(false);
+
   const handleAddFriendClick = () => {
     fetch(`/api/friendreqs/${user.appUserID}/sendFriendRequestByUsername/${username}`, {
-    method: 'PUT'
-  });
+      method: 'PUT'
+    }).then(() => setSent(true));
   };
-  return <button onClick={handleAddFriendClick}>Add Friend</button>;
+
+  return (
+    <>
+      {sent ? 'Friend request sent!' : <button onClick={handleAddFriendClick}>Add Friend</button>}
+    </>
+  );
 };
 
 export default SendFriendRequestButton;
