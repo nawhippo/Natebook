e6a78fff50df72266914bba3d7980224dc807e0d
NATHAN <whipmiester@hotmail.com>
2023-10-29 22:48:06 -0400

    updated security config, should work now

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/SecurityConfig.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/SecurityConfig.java
@@ -1,18 +1,45 @@
 package SoloProject.SocialMediaApp;
+
+import SoloProject.SocialMediaApp.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    // Define a bean of type PasswordEncoder
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-                .csrf().disable();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        http
+                .authenticationProvider(daoAuthenticationProvider(passwordEncoder))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/api/login","/api/createAccount").permitAll()
+                        .anyRequest().authenticated()
+                );
         return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 }
