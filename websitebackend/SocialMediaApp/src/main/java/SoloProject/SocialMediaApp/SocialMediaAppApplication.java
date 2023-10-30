b6206da7e4ec7cf5eef1297790aec1531dc68d93
NATHAN <whipmiester@hotmail.com>
2023-10-29 22:52:14 -0400

    add models

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/SocialMediaAppApplication.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/SocialMediaAppApplication.java
@@ -1,11 +1,11 @@
 package SoloProject.SocialMediaApp;
 
-import org.apache.catalina.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 @SpringBootApplication
+@EntityScan(basePackages = "SoloProject.SocialMediaApp.models")
 public class SocialMediaAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaAppApplication.class, args);
