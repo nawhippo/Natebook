202499cc01cb524e852bbfc9806adc218fbe9d39
NATHAN <whipmiester@hotmail.com>
2023-10-29 22:37:08 -0400

    reworked display

--- websitefrontend2/websitefrontend/src/pages/feedPage/feedPage.js
+++ websitefrontend2/websitefrontend/src/pages/feedPage/feedPage.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
-import PublicFeed from './components/publicFeed'; 
+import PublicFeed from './components/publicFeed';
 import FriendFeed from './components/friendFeed';
 
-const FeedPage = () => {
-  const [showFriendFeed, setShowFriendFeed] = useState(false);
+const FeedPage = ({ userContext }) => {
+    const [showFriendFeed, setShowFriendFeed] = useState(false);
 
-  return (
-    <div>
-      <button onClick={() => setShowFriendFeed(false)}>All Posts</button>
-      <button onClick={() => setShowFriendFeed(true)}>Friend Posts</button>
+    return (
+        <div>
+            {userContext && (
+                <>
+                    <button onClick={() => setShowFriendFeed(false)}>All Posts</button>
+                    <button onClick={() => setShowFriendFeed(true)}>Friend Posts</button>
+                </>
+            )}
 
-      {showFriendFeed ? <FriendFeed /> : <PublicFeed />}
-    </div>
-  );
+            {showFriendFeed && userContext ? <FriendFeed /> : <PublicFeed />}
+        </div>
+    );
 };
 
 export default FeedPage;
