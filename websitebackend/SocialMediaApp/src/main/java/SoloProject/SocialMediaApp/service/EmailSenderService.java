c013b366b6dbc5200a5a35b6ddd75d6886442506
NATHAN <whipmiester@hotmail.com>
2023-10-29 22:35:39 -0400

    refactored

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/service/EmailSenderService.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/service/EmailSenderService.java
@@ -1,27 +1,33 @@
 package SoloProject.SocialMediaApp.service;
+
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import java.util.Random;
 
+@Service
 public class EmailSenderService {
 
     private Properties properties;
+
     public ResponseEntity<?> sendEmail(String userEmail) {
         String to = userEmail;
-        String from = "nateapplications@gmail.com";
-        String password = "wwjx fxad xcfn xtww";
+        String from = "...";
+        String password = "...";
 
-        String host = "localhost";
+        String host = "smtp.gmail.com"; // Use Gmail's SMTP server
         Properties properties = new Properties();
         properties.setProperty("mail.smtp.host", host);
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
 
-        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+        Session session = Session.getInstance(properties, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(from, password);
             }
@@ -32,14 +38,22 @@ public class EmailSenderService {
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject("Recovery Password");
 
-            String recoveryPassword = "your_recovery_password";
+            String recoveryPassword = generateOTP();
             message.setText("This is your recovery password: " + recoveryPassword);
 
             Transport.send(message);
             System.out.println("Sent Message Successfully");
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return ResponseEntity.ok("Email sent successfully to " + userEmail);
+    }
+
+    //generate 6 digit otp
+    public String generateOTP() {
+        Random random = new Random();
+        int otp = 100000 + random.nextInt(900000);
+        return Integer.toString(otp);
     }
 }
