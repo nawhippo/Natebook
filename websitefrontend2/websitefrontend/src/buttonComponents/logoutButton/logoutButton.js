f93338d0cdd5eecc8fb70804953f4a5e385ae721
NATHAN <whipmiester@hotmail.com>
2023-10-29 22:40:43 -0400

    added further css

--- websitefrontend2/websitefrontend/src/buttonComponents/logoutButton/logoutButton.js
+++ websitefrontend2/websitefrontend/src/buttonComponents/logoutButton/logoutButton.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
 import { useUserContext } from '../../pages/usercontext/UserContext';
 import Cookies from 'js-cookie';
 
 const LogoutButton = () => {
-  const { clearUserContext } = useUserContext();
+  const {user, clearUserContext } = useUserContext();
   const history = useHistory();
-
+  const [error, setError] = useState();
+  const [buttonUsed, setButtonUsed] = useState('');
   const handleLogout = () => {
+    const fetch = ((`/api/${user.appUserID}/logout`), {
+      Method: "PUT"})
+      .then(response => {
+        if(!response.ok){
+          throw new Error('Response was not ok!')
+    }
+          console.log('Logged out successfully');
+          setButtonUsed('Logged out successfully');
+        })
+    .catch(error => {
+    console.log('Logout failed');
+    setButtonUsed('Logout failed');
+    });
+    };
+
     clearUserContext();
     Cookies.remove('userData');
     history.push('/Feed');
-  };
 
-  return (
+  return(
+      <div>
     <button onClick={handleLogout}>Logout</button>
+        {buttonUsed && <p>{buttonUsed}</p>}
+      </div>
   );
 };
-
 export default LogoutButton;
