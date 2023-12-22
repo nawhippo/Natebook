156d81748ac6bd2af86304079625abe8b02b431b
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 16:53:51 -0500

    Frontend

--- websitefrontend2/websitefrontend/src/buttonComponents/deleteCommentButton/deleteCommentButton.js
+++ websitefrontend2/websitefrontend/src/buttonComponents/deleteCommentButton/deleteCommentButton.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {useUserContext} from "../../pages/usercontext/UserContext";
 
-export const DeleteCommentButton = ({ posterusername, postId, commentId, targetUsername, fetchData }) => {
+export const DeleteCommentButton = ({ commentId }) => {
   const user = useUserContext();
 
+
   const buttonStyle = {
-    backgroundColor: user && user.backgroundColor ? user.backgroundColor : 'orange'
+    backgroundColor: user && user.backgroundColor ? user.backgroundColor : 'grey',
+    color: '#FFFFFF',
+    border: '4px solid black',
   };
+
   const handleDeleteComment = async () => {
-    console.log(typeof posterusername, typeof postId, typeof commentId);
     try {
-      const response = await fetch(`http://localhost:3000/api/post/${posterusername}/${postId}/${commentId}/deleteComment`, { method: 'DELETE' });
+      const response = await fetch(`/api/${commentId}/deleteComment`,
+          { method: 'DELETE' });
       if (!response.ok) {
         throw new Error('Failed to delete.');
       }
-      fetchData(targetUsername);
     } catch (error) {
       return error.message;
     }
