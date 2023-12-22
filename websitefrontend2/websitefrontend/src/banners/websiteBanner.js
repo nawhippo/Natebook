df43fd7196bbbb33bda3f2a10c415a5f9935429d
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 17:21:52 -0500

    Frontend

--- websitefrontend2/websitefrontend/src/banners/websiteBanner.js
+++ websitefrontend2/websitefrontend/src/banners/websiteBanner.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import React, {useEffect, useState} from "react";
+import {useHistory} from 'react-router-dom/cjs/react-router-dom.min';
 import LoginButton from '../buttonComponents/loginButton/loginButton';
 import LogoutButton from '../buttonComponents/logoutButton/logoutButton';
 import CreateAccount from '../buttonComponents/createAccountButton/createAccountButton';
-import FriendReqCounter from './notificationHelpers/friendRequestHelpers';
+// import FriendReqCounter from './notificationHelpers/friendRequestHelpers';
 import SettingsIcon from '@mui/icons-material/Settings';
 import PeopleIcon from '@mui/icons-material/People';
 import MessageIcon from '@mui/icons-material/Message';
@@ -11,7 +11,7 @@ import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import ProfilePictureComponent from '../buttonComponents/ProfilePictureComponent';
 import './banner.css';
-import { useUserContext } from "../pages/usercontext/UserContext";
+import {useUserContext} from "../pages/usercontext/UserContext";
 import Cookies from 'js-cookie';
 
 const Banner = () => {
@@ -31,7 +31,7 @@ const Banner = () => {
     };
 
     const bannerStyle = {
-        background: user && user.backgroundColor ? `linear-gradient(to right, ${user.backgroundColor}, black)` : 'linear-gradient(to right, orange, black)'
+        background: user && user.backgroundColor ? `linear-gradient(to right, ${user.backgroundColor}, black)` : 'linear-gradient(to right, grey, black)'
     };
 
     return (
@@ -39,7 +39,7 @@ const Banner = () => {
             <h1 className="title">NateBook</h1>
             <PeopleIcon className="button-common" onClick={() => handleLinkClick('/AllUsersPage')} style={{ width: '50px', height: 'auto', background: 'none' }} />
             <DynamicFeedIcon className="button-common" onClick={() => handleLinkClick('/Feed')} style={{ width: '50px', height: 'auto', background: 'none' }} />
-            <FriendReqCounter style={{width:'50px', background:'red', color:'white'}}/>
+            {/*<FriendReqCounter style={{width:'50px', background:'red', color:'white'}}/>*/}
             {userData ? (
                 <div>
                     <MessageIcon className="button-common" onClick={() => handleLinkClick('/Messages')} style={{ width: '50px', height: 'auto', background: 'none' }} />
