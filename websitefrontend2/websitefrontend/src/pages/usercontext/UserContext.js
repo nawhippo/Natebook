2754df3be7c96135926617648f26de22c6108412
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 17:20:56 -0500

    Frontend

--- websitefrontend2/websitefrontend/src/pages/usercontext/UserContext.js
+++ websitefrontend2/websitefrontend/src/pages/usercontext/UserContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, {createContext, useContext, useEffect, useState} from 'react';
 import Cookies from 'js-cookie';
 
 const UserContext = createContext();
