14e68d4bcd33993863bc4adcd3fdcd76264012c4
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 17:08:35 -0500

    Frontend

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/models/MessageThread.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/models/MessageThread.java
@@ -32,14 +32,29 @@ public class MessageThread {
 
     }
 
-    public MessageThread(List<Long> participants) {
+    public MessageThread(List<Long> participants, List<String> participantsNanes) {
         this.participants = participants;
+        this.participantsNames = participantsNames;
     }
     @ElementCollection
     List<Long> participants;
 
+    public List<String> getParticipantsNames() {
+        return participantsNames;
+    }
+
+    @ElementCollection
+    List<String> participantsNames;
+
+    public MessageThread(List<String> participantsNames) {
+        this.participantsNames = participantsNames;
+    }
     public MessageThread(Long id, List<Long> participants) {
         this.id = id;
         this.participants = participants;
+    }
+
+    public void setParticipantsNames(List<String> recipientusernames) {
+        this.participantsNames = recipientusernames;
     }
 }
