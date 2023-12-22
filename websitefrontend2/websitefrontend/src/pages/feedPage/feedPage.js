9f9f23ff55f93a36ca04d28b66bdfd953385ef89
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 16:55:56 -0500

    Frontend

--- websitefrontend2/websitefrontend/src/pages/feedPage/feedPage.js
+++ websitefrontend2/websitefrontend/src/pages/feedPage/feedPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import PublicFeed from './components/publicFeed';
 import FriendFeed from './components/friendFeed';
 
