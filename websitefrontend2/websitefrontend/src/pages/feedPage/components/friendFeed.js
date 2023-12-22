3e2f9c4e9bc3808b1a960f59ab8bcce6af7d906c
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 16:57:14 -0500

    Frontend

--- websitefrontend2/websitefrontend/src/pages/feedPage/components/friendFeed.js
+++ websitefrontend2/websitefrontend/src/pages/feedPage/components/friendFeed.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import Post from '../../objects/post';
 import CreatePostButton from '../../../buttonComponents/createPostButton/createPostButton';
-import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import '../../../global.css';
-import { useUserContext } from '../../usercontext/UserContext';
+import {useUserContext} from '../../usercontext/UserContext';
 
 const FriendFeed = () => {
   const { user } = useUserContext();
