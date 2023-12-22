167b3c26fb70838268cc2ae6fa811287c6b10de6
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 17:08:47 -0500

    Frontend

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/repository/MessageThreadRepository.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/repository/MessageThreadRepository.java
@@ -9,14 +9,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MessageThreadRepository extends JpaRepository<MessageThread, Long> {
-
-    @Query("SELECT t FROM MessageThread t " +
-            "WHERE :participantIds MEMBER OF t.participants " +
-            "GROUP BY t " +
-            "HAVING COUNT(t.participants) = :participantCount " +
-            "AND COUNT(t.participants) = (SELECT COUNT(tp) FROM MessageThread t2 JOIN t2.participants tp WHERE t2 = t)")
-    Optional<MessageThread> findMessageThreadByExactParticipants(
-            @Param("participantIds") List<Long> participantIds,
-            @Param("participantCount") long participantCount
-    );
+    List<MessageThread> findByParticipantsContains(Long participantId);
 }
