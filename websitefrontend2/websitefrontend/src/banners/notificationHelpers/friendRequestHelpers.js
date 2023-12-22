40d73ce0a752c7edca17aa897ad27bf140e8a7f5
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2023-12-22 16:57:27 -0500

    Frontend

--- websitefrontend2/websitefrontend/src/banners/notificationHelpers/friendRequestHelpers.js
+++ websitefrontend2/websitefrontend/src/banners/notificationHelpers/friendRequestHelpers.js
@@ -1,37 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { useUserContext } from '../../pages/usercontext/UserContext';
-const FriendReqCounter = () => {
-  const { user } = useUserContext();
-  const [allFriendRequestsData, setAllFriendRequestsData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchFriendRequests = () => {
-      if (user) {
-        fetch(`/api/friendreqs/${user.appUserID}/getFriendRequests`)
-          .then(response => response.json())
-          .then(data => setAllFriendRequestsData(data))
-          .catch(error => setError(error.message))
-          .finally(() => setIsLoading(false));
-      }
-    };
-
-    //init fetch
-    fetchFriendRequests();
-
-    //poll rate is 2 mins
-    const intervalId = setInterval(fetchFriendRequests, 120000);
-    //clean up
-    return () => clearInterval(intervalId);
-
-  }, [user]);
-
-  return (
-    <div>
-      {allFriendRequestsData.length > 0 && <p>Number of Friend Requests: {allFriendRequestsData.length}</p>}
-    </div>
-  );
-};
-
-export default FriendReqCounter;
+// import React, { useState, useEffect } from 'react';
+// import { useUserContext } from '../../pages/usercontext/UserContext';
+// import { debounce } from 'lodash';
+//
+// const FriendReqCounter = () => {
+//   const { user } = useUserContext();
+//   const [allFriendRequestsData, setAllFriendRequestsData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//
+//   const fetchwithCsrfFriendRequests = () => {
+//     if (user) {
+//       fetchwithCsrf(`/api/friendreqs/${user.appUserID}/getFriendRequests`)
+//           .then(response => response.json())
+//           .then(data => setAllFriendRequestsData(data))
+//           .catch(error => setError(error.message))
+//           .finally(() => setIsLoading(false));
+//     }
+//   };
+//
+//   // Initial fetchwithCsrf
+//   fetchwithCsrfFriendRequests();
+//
+//   // Clear any existing intervals
+//   const intervalId = setInterval(fetchwithCsrfFriendRequests, 120000);
+//
+//   // Clean up previous intervals
+//   useEffect(() => {
+//     return () => clearInterval(intervalId);
+//   }, [intervalId]);
+//
+//   return (
+//       <div>
+//         {allFriendRequestsData.length > 0 && <p>Number of Friend Requests: {allFriendRequestsData.length}</p>}
+//       </div>
+//   );
+// };
+//
+// export default FriendReqCounter;
