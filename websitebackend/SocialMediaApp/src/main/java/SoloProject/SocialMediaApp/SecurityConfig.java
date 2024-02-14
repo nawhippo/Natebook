db4af6bd4c544664890998194e3036fe752c4373
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2024-02-14 12:18:16 -0500

    Altered allowed origins.

--- websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/SecurityConfig.java
+++ websitebackend/SocialMediaApp/src/main/java/SoloProject/SocialMediaApp/SecurityConfig.java
@@ -187,7 +187,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://natebookfrontend.onrender.com","https://localhost:3000", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://natebookfrontend.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
